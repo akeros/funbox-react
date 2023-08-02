e8349b45234a4b4fabbafd98f486d10ebc92b04f
akeros <mixali4kin@bk.ru>
2023-08-02 14:34:43 +0400

    start testProject

---
+++ src/components/card.js
@@ -0,0 +1,31 @@
+import "../styles/card.css"
+import cat from "../img/catBarsik.png"
+import {useState} from "react";
+
+export const Card = () => {
+    const[selected,setSelected] = useState(false)
+    return (
+        <div className="cardName-main">
+            <div className={!selected ? "cardName" : "cardNameRed"} onClick={()=> setSelected(prevState => !prevState)}>
+                <div className={!selected ? "cardName-linear" : "cardName-linearRed"} />
+                <div className={!selected ? "cardName-weight" : "cardName-weightRed"}>
+                    <div className="cardName-weight_number">0,5</div>
+                    <div className="cardName-weight_string">кг</div>
+                </div>
+                <img className="cardName-catImage" src={cat} alt={"Красивый кот"}/>
+                <div className="cardName-title">
+                    <div className="cardName-title_low">Сказочное заморское яство</div>
+                    <div className="cardName-title_high">Нямушка</div>
+                    <div className="cardName-title_medium">с фуа-гра</div>
+                </div>
+            </div>
+            {!selected
+                ? (
+                <div className="footerCard">Чего сидишь? Порадуй котэ,<span className="footerCard-link"> купи.</span> </div>
+            ) : (
+                <div className="footerCard">Печень утки разварная с артишоками.</div>
+                )}
+
+        </div>
+    )
+}
